c265b1ca390d47e1ca9fb8ecc28f6ecd5ccde361
RichardDorian <richard.dorian@outlook.com>
2022-10-19 18:50:11 +0200

    fix: :bug: fix typing error

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,12 @@
 /// <reference path="algorithms.d.ts" />
 /// <reference path="errors.d.ts" />
 /// <reference path="structures.d.ts" />
+/// <reference path="validation.d.ts" />
 
 declare module 'nodejs-vitals' {
   import * as err from 'nodejs-vitals/errors';
-  export const errors = err;
+  export const errors: typeof err;
   export * from 'nodejs-vitals/algorithms';
   export * from 'nodejs-vitals/structures';
+  export * from 'nodejs-vitals/validation';
 }
