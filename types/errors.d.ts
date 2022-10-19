6996c00f2176fe9a7e33f28d499b54e787f7f946
RichardDorian <richard.dorian@outlook.com>
2022-10-19 21:51:31 +0200

    chore: :label: add missing type definition

--- types/errors.d.ts
+++ types/errors.d.ts
@@ -2,4 +2,7 @@ declare module 'nodejs-vitals/errors' {
   export class QueueOverflowError extends Error {
     public constructor(maxSize: number);
   }
+  export class StackOverflowError extends Error {
+    public constructor(maxSize: number);
+  }
 }
